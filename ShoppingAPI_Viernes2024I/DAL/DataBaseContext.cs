417f1a653a27c97c577a41cbee0f9419c82bae2b
Ycamargo2611 <145471096+Ycamargo2611@users.noreply.github.com>
2024-11-08 14:08:36 -0500

    Set up DB configuration and Creation

---
+++ ShoppingAPI_Viernes2024I/DAL/DataBaseContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingAPI_Viernes2024I.DAL.Entities;
+
+namespace ShoppingAPI_Viernes2024I.DAL
+{
+    public class DataBaseContext:DbContext
+    {
+        //asi me conecto a la bse de datos, por medio de este constructor
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+
+        //este metodo es propio de entity Framework, me sirve para configurar unos indies de cada campo en la BD
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();// creo indice del campo name para tabla countries
+        }
+
+        #region DbSets
+
+        public DbSet<Country> Countries { get; set; }
+
+
+        #endregion
+
+    }
+
+
+}
---
+++ ShoppingAPI_Viernes2024I/DAL/Entities/Country.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingAPI_Viernes2024I.DAL.Entities
+{
+    public class Country:AuditBase
+    {
+        [Display (Name= "Pais")]//identificar nombre mas facil
+        [MaxLength(50, ErrorMessage = "el campo {0} debe tener maximo {1} caracteres ")] //longitud Maxima
+        [Required (ErrorMessage = "Es campo {0} es obligatorio ")] //Campo Obligatoria
+        public string Name { get; set; }
+    }
+}
