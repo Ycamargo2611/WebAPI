7b3a079c04adf41ebd0b3ad329bb3d82c7b091e9
Ycamargo2611 <145471096+Ycamargo2611@users.noreply.github.com>
2024-11-10 16:13:49 -0500

    CountryService : ICountryService

--- ShoppingAPI_Viernes2024I/Domain/Services/CountryService.cs
+++ ShoppingAPI_Viernes2024I/Domain/Services/CountryService.cs
@@ -1,6 +1,33 @@
-﻿namespace ShoppingAPI_Viernes2024I.Domain.Services
+﻿using ShoppingAPI_Viernes2024I.DAL.Entities;
+using ShoppingAPI_Viernes2024I.Domain.Interfaces;
+
+namespace ShoppingAPI_Viernes2024I.Domain.Services
 {
-    public class CountryService
+    public class CountryService : ICountryService
     {
-    }
+        public Task<Country> CreateCountryAsync(Country country)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Country> DeleteCountryAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Country> EditCountryAsync(Country country)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Country>> GetCountriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Country> GetCountryByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }//aca empiezas
 }
