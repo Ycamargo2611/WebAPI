94262cec4d14258e14626278b00f628a5e0c4e76
Andrés Giraldo <afgiraldoestrada@gmail.com>
2024-11-11 18:52:22 -0500

    Creacion de clases (video 4 y 5)

--- ShoppingAPI_Viernes2024I/Domain/Services/CountryService.cs
+++ ShoppingAPI_Viernes2024I/Domain/Services/CountryService.cs
@@ -1,4 +1,6 @@
-﻿using ShoppingAPI_Viernes2024I.DAL.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingAPI_Viernes2024I.DAL;
+using ShoppingAPI_Viernes2024I.DAL.Entities;
 using ShoppingAPI_Viernes2024I.Domain.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using ShoppingAPI_Viernes2024I.DAL;
@@ -7,29 +9,99 @@ namespace ShoppingAPI_Viernes2024I.Domain.Services
 {
     public class CountryService : ICountryService
     {
-        public Task<Country> CreateCountryAsync(Country country)
+        private readonly DataBaseContext _context;
+
+        public CountryService(DataBaseContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public Task<Country> DeleteCountryAsync(Guid id)
+        public async Task<IEnumerable<Country>> GetCountriesAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var countries = await _context.Countries.ToListAsync();
+                return countries;
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+
+                throw new Exception(dbUpdateException.InnerException?.Message ?? dbUpdateException.Message);
+            }
+        }
+        public async Task<Country> GetCountryByIdAsync(Guid id)
+        {
+            try
+            {
+                var country = await _context.Countries.FirstOrDefaultAsync(c => c.id == id);
+                return country;
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+
+                throw new Exception(dbUpdateException.InnerException?.Message ?? dbUpdateException.Message);
+            }
         }
 
-        public Task<Country> EditCountryAsync(Country country)
+        public async Task<Country> CreateCountryAsync(Country country)
         {
-            throw new NotImplementedException();
+            try
+            {
+                country.id = Guid.NewGuid();
+                country.CreatedDate = DateTime.Now;
+                _context.Countries.Add(country); //El metodo Add() me permite crear el objeto en el contexto de la BD
+
+                await _context.SaveChangesAsync(); //Guarda el pais en la tabla CONTRY
+
+                return country;
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+
+                throw new Exception(dbUpdateException.InnerException?.Message ?? dbUpdateException.Message);
+            }
         }
 
-        public Task<IEnumerable<Country>> GetCountriesAsync()
+
+        public async Task<Country> EditCountryAsync(Country country)
         {
-            throw new NotImplementedException();
+            try
+            {
+                country.ModifiedDate = DateTime.Now;
+                _context.Countries.Update(country); //Virtualizo mi objeto
+                await _context.SaveChangesAsync(); //Guarda el pais en la tabla CONTRY
+
+                return country;
+
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+
+                throw new Exception(dbUpdateException.InnerException?.Message ?? dbUpdateException.Message);
+            }
         }
 
-        public Task<Country> GetCountryByIdAsync(Guid id)
+        public async Task<Country> DeleteCountryAsync(Guid id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var country = await GetCountryByIdAsync(id);
+
+                if (country == null)
+                {
+                    return null;
+                }
+
+                _context.Countries.Remove(country); //Query
+                await _context.SaveChangesAsync(); //Ejecución del query
+
+                return country;
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+
+                throw new Exception(dbUpdateException.InnerException?.Message ?? dbUpdateException.Message);
+            }
         }
-    }//aca empiezas pruebaaaa
+    }
 }
--- ShoppingAPI_Viernes2024I/Program.cs
+++ ShoppingAPI_Viernes2024I/Program.cs
@@ -12,6 +12,11 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataBaseContext>( o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddTransient<SeederDB>();
 
+//Contenedor de dependencias (Creando el servicio para poder ser ultizado en el controlador
+builder.Services.AddScoped<ICountryService, CountryService>();
+builder.Services.AddTransient<ICountryService, CountryService>();
+builder.Services.AddSingleton<ICountryService, CountryService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
